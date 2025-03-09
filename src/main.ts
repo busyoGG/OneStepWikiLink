9a936e35f6f0a6ace26c68f9134381792e57d10a
朽朽朽朽朽 <694253220@qq.com>
2025-03-09 20:08:31 +0800

    完成核心功能

---
+++ src/main.ts
@@ -0,0 +1,351 @@
+import { Editor, MarkdownFileInfo, MarkdownView, Plugin, TFile, View } from "obsidian";
+import { OneStepWikiLinkPluginSettingTab } from "./setting";
+
+const path = require("path");
+
+export enum Language {
+	CN = "CN",
+	EN = "EN"
+}
+
+interface OneStepWikiLinkPluginSettings {
+	showDetails: boolean;
+	autoConvert: boolean;
+	language: Language;
+	NonBoundaryCheckers: string[];
+	excludes: string[];
+}
+
+const DEFAULT_SETTINGS: OneStepWikiLinkPluginSettings = {
+	showDetails: true,
+	autoConvert: false,
+	language: Language.CN,
+	NonBoundaryCheckers: ["Han", "Hiragana", "Katakana", "Hangul"],
+	excludes: []
+}
+
+export default class OneStepWikiLinkPlugin extends Plugin {
+
+	settings: OneStepWikiLinkPluginSettings;
+
+	// ----- 私有变量 -----
+
+	fileNameList: string[] = [];
+
+	currentFileName: string = "";
+
+	btnOneStep: HTMLDivElement | undefined;
+
+	divForDetails: HTMLDivElement | undefined;
+
+	openEditor: Editor | undefined;
+
+	matchingFiles: string[] = [];
+
+	labels = {
+		btnOneStep: {
+			[Language.CN]: "全部转换为维基链接",
+			[Language.EN]: "Convert All to Wiki Links"
+		}
+	}
+
+	async onload() {
+		await this.loadSettings();
+
+		this.addSettingTab(new OneStepWikiLinkPluginSettingTab(this.app, this));
+
+		this.app.workspace.onLayoutReady(() => {
+			this.init();
+		});
+	}
+
+	onunload() {
+		this.btnOneStep?.remove();
+		this.divForDetails?.remove();
+	}
+
+	async init() {
+		//初始化文件名列表
+		this.getAllFileNames();
+
+		//获取当前编辑器
+		this.openEditor = this.app.workspace.getActiveViewOfType(MarkdownView)?.editor;
+
+		//初始化按钮
+		let outlinkPanel = this.app.workspace.getLeavesOfType("outgoing-link")[0];
+		if (outlinkPanel && !this.btnOneStep) {
+			this.createButton(outlinkPanel.view.containerEl);
+			if (!this.openEditor) {
+				this.btnOneStep && (this.btnOneStep as HTMLDivElement).addClass("hide");
+				this.divForDetails && (this.divForDetails as HTMLDivElement).addClass("hide");
+			}
+		}
+
+		this.registerEvent(this.app.workspace.on("active-leaf-change", (leaf) => {
+
+			//监听页面变化，防止出链界面关闭后再打开按钮消失
+			let outlinkPanel = this.app.workspace.getLeavesOfType("outgoing-link")[0];
+
+			if (outlinkPanel) {
+				if (!this.btnOneStep) {
+					this.createButton(outlinkPanel.view.containerEl);
+				}
+			} else {
+				this.btnOneStep = undefined;
+			}
+
+			let type = leaf?.view.getViewType();
+			if (leaf) {
+				//监听当前激活的编辑器
+				if (type === "markdown") {
+					this.openEditor = leaf.view.app.workspace.activeEditor?.editor;
+					// console.log(this.openEditor);
+					if (this.openEditor) {
+						this.checkContent(this.openEditor.getValue());
+					}
+				} else if (type == "empty") {
+					this.btnOneStep?.addClass("hide");
+					this.divForDetails?.addClass("hide");
+				}
+			}
+		}));
+
+
+		let file = this.app.workspace.getActiveFile();
+		this.openEditor = this.app.workspace.getActiveViewOfType(MarkdownView)?.editor;
+
+		if (file && this.openEditor) {
+			this.currentFileName = (file as TFile).basename;
+
+			let content = await this.app.vault.read(file);
+			this.checkContent(content);
+		}
+
+		this.addCommand({
+			id: "convert-all-matching-words-to-wiki-links",
+			name: "Convert All Matching Words to Wiki Links",
+			editorCallback: (editor) => {
+				this.convert2WikiLink();
+			}
+		});
+
+		this.addCommand({
+			id: "convert-all-matching-words-to-wiki-links-cn",
+			name: "转换所有匹配的单词为维基链接",
+			editorCallback: (editor) => {
+				this.convert2WikiLink();
+			}
+		});
+
+
+		//监听文本变化
+		this.registerEvent(this.app.workspace.on("editor-change", async (file, data) => {
+			// console.log(file, data);
+			if (file) {
+				this.currentFileName = (data.file as TFile).basename;
+				// console.log(file.getValue());
+				this.checkContent(file.getValue());
+			}
+		}));
+
+		this.registerEvent(this.app.vault.on("rename", (file, oldPath) => {
+			this.updateFileNameList((file as TFile).basename, true, oldPath.replace(".md", ""));
+		}));
+
+		this.registerEvent(this.app.vault.on("delete", (file) => {
+			this.updateFileNameList((file as TFile).basename, false);
+		}));
+
+		// this.registerEvent(this.app.workspace.on("file-open", async (file) => {
+		// 	// console.log("file open", file);
+		// 	this.openEditor = this.app.workspace.getActiveViewOfType(MarkdownView)?.editor;
+
+		// 	if (file) {
+		// 		this.currentFileName = (file as TFile).basename;
+
+		// 		let content = await this.app.vault.read(file);
+		// 		this.checkContent(content);
+		// 	}
+		// }));
+
+		console.log(this.fileNameList)
+
+		// this.checkContent();
+	}
+
+	getAllFileNames() {
+		this.fileNameList = [];
+
+		let files = this.app.vault.getMarkdownFiles();
+		for (let file of files) {
+
+			let checkFileName = this.settings.excludes.includes(file.basename);
+			let checkFilePath;
+			for (let exclude of this.settings.excludes) {
+				let filePath = path.dirname(file.path) + "/";
+				if (exclude.endsWith("/") && filePath.startsWith(exclude)) {
+					checkFilePath = true;
+					break;
+				}
+			}
+
+			if (checkFileName || checkFilePath) {
+				continue;
+			}
+
+			this.fileNameList.push(file.basename);
+		}
+	}
+
+	updateFileNameList(name: string, add: boolean, extra: string = "") {
+
+		if (add) {
+			let index = this.fileNameList.indexOf(extra);
+			if (index >= 0) {
+				this.fileNameList[index] = name;
+			} else {
+				this.fileNameList.push(name);
+			}
+		} else {
+			this.fileNameList.splice(this.fileNameList.indexOf(name), 1);
+		}
+	}
+
+	checkContent(data: string) {
+
+		this.matchingFiles = [];
+
+		if (this.divForDetails) {
+			this.divForDetails.empty();
+		}
+
+		const contentWithoutLinks = data.replace(/\[\[([^\[\]]+)\]\]/g, "");
+
+		this.fileNameList.forEach(fileName => {
+			// 排除当前文件
+			if (fileName !== this.currentFileName && contentWithoutLinks.includes(fileName)) {
+				this.matchingFiles.push(fileName);
+
+				//添加详情
+				if (this.divForDetails) {
+					this.divForDetails.createDiv({
+						cls: "one-step-wikilink-detail-busyo",
+						text: fileName
+					});
+				}
+			}
+		});
+
+		this.matchingFiles.sort((a, b) => a.length > b.length ? -1 : 1);
+
+		if (this.matchingFiles.length > 0) {
+
+			if (this.settings.autoConvert) {
+				this.btnOneStep?.addClass("hide");
+				this.divForDetails?.addClass("hide");
+				this.convert2WikiLink();
+			} else {
+				this.btnOneStep?.removeClass("hide");
+				this.settings.showDetails && this.divForDetails?.removeClass("hide");
+			}
+		} else {
+			this.btnOneStep?.addClass("hide");
+			this.divForDetails?.addClass("hide");
+		}
+
+	}
+
+	/** 转换所有匹配项为维基链接 */
+	convert2WikiLink() {
+		if (this.openEditor) {
+			let data = this.openEditor.getValue();
+
+			let changes = [];
+
+			for (const match of this.matchingFiles) {
+				let regex = new RegExp(`(?<!\\[\\[)${match}\\b(?!\\]\\])`, "g");
+				//检测字符串最后一个字符是否为有边界的语言
+				if (this.isNonBoundaryChar(match.charAt(match.length - 1))) {
+					regex = new RegExp(`(?<!\\[\\[)${match}(?!\\]\\])`, "g");
+				}
+
+				let res
+				while ((res = regex.exec(data)) !== null) {
+
+					let pos = this.openEditor.offsetToPos(res.index);
+					let endPos = { ch: pos.ch + match.length, line: pos.line };
+
+					changes.push({
+						from: pos,
+						to: endPos,
+						text: `[[${match}]]`
+					});
+
+					// data = data.replace(regex, `[[${match}]]`);
+				}
+			}
+
+			this.openEditor.transaction({ changes: changes });
+		}
+	}
+
+	// 判断字符是否属于有边界的语言（如英文、法文等）
+	isNonBoundaryChar(char: string) {
+		if (this.settings.NonBoundaryCheckers.length === 0) return false;
+
+		let checkers = this.settings.NonBoundaryCheckers
+			.map(checker => `\\p{Script=${checker}}`)  // 生成 Unicode Script 规则
+			.join("|");  // 以 | 分隔
+
+		return new RegExp(checkers, "u").test(char);
+	}
+
+	createButton(root: Element) {
+		let container = root.querySelector(".outgoing-link-pane");
+		if (container) {
+			this.btnOneStep = container.createDiv({
+				cls: "one-step-wikilink-container-busyo",
+				text: this.labels.btnOneStep[this.settings.language]
+			});
+			container.insertBefore(this.btnOneStep, container.firstChild);
+
+			this.btnOneStep.onclick = () => {
+				this.convert2WikiLink();
+			};
+
+			this.divForDetails = container.createDiv({
+				cls: "one-step-wikilink-detail-container-busyo"
+			});
+
+			container.insertBefore(this.divForDetails, container.firstChild);
+
+			if (!this.settings.showDetails) {
+				this.divForDetails.addClass("hide");
+			}
+		}
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+
+		//保存配置的时候检测是否需要显示目标 Div
+		if (this.matchingFiles.length > 0) {
+			!this.settings.showDetails && this.divForDetails?.removeClass("hide");
+		} else {
+			this.divForDetails?.addClass("hide");
+		}
+
+		//更改语言
+		this.btnOneStep?.setText(this.labels.btnOneStep[this.settings.language]);
+
+		//更新文件列表
+		this.getAllFileNames();
+		if (this.openEditor) {
+			this.checkContent(this.openEditor.getValue());
+		}
+	}
+}
---
+++ src/setting.ts
@@ -0,0 +1,167 @@
+import OneStepWikiLinkPlugin from "src/main";
+import { App, PluginSettingTab, Setting, View, WorkspaceLeaf } from "obsidian";
+import { Language } from "src/main";
+
+export class OneStepWikiLinkPluginSettingTab extends PluginSettingTab {
+    plugin: OneStepWikiLinkPlugin;
+
+    labels = {
+        details: {
+            name: {
+                [Language.CN]: "详情开关",
+                [Language.EN]: "Details Switch"
+            },
+            desc: {
+                [Language.CN]: "是否显示所有匹配的内容",
+                [Language.EN]: "Whether to display all matching content"
+            }
+        },
+        autoConvert: {
+            name: {
+                [Language.CN]: "自动转换开关",
+                [Language.EN]: "Auto Convert Switch"
+            },
+            desc: {
+                [Language.CN]: "是否自动转换所有匹配的内容",
+                [Language.EN]: "Whether to automatically convert all matching content"
+            }
+        },
+        NonBoundaryCheckers: {
+            name: {
+                [Language.CN]: "非边界字符",
+                [Language.EN]: "Non-Boundary Characters"
+            },
+            desc: {
+                [Language.CN]: "用于检测没有边界的字符，如汉字，以 `，` 或 `,` 分隔，值为正则表达式的 `Script=Han` 形式中的 Han 部分",
+                [Language.EN]: "Used to detect characters without boundaries, such as Chinese characters, separated by `，` or `,` , and the `Script=Han` part of the regular expression"
+            }
+        },
+        excludes: {
+            name: {
+                [Language.CN]: "排除列表",
+                [Language.EN]: "Exclude List"
+            },
+            desc: {
+                [Language.CN]: "不检测排除列表中的文件，以 `，` 或 `,` 分隔，不带后缀，排除文件夹需要输入文件夹的相对路径并以 `/` 结尾",
+                [Language.EN]: "Do not check files in the exclude list, separated by `，` or `,`, without suffix, exclude folders need to enter the relative path of the folder and end with `/`"
+            }
+        }
+    };
+
+    settings: {
+        key: {
+            name: {
+                CN: string;
+                EN: string;
+            };
+            desc: {
+                CN: string;
+                EN: string;
+            };
+        }; value: Setting;
+    }[] = [];
+
+    constructor(app: App, plugin: OneStepWikiLinkPlugin) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
+
+    display(): void {
+        let { containerEl } = this;
+
+        containerEl.empty();
+
+        new Setting(containerEl)
+            .setName("语言 Language")
+            .setDesc("选择语言 Choose language")
+            .addDropdown((dropdown) =>
+                dropdown
+                    .addOption("CN", "CN")
+                    .addOption("EN", "EN")
+                    .setValue(this.plugin.settings.language)
+                    .onChange(async (value) => {
+                        this.plugin.settings.language = value as Language;
+                        await this.plugin.saveSettings();
+
+                        this.updateLanguage();
+                    })
+            );
+
+        this.settings.push(
+            {
+                key: this.labels.details,
+                value: new Setting(containerEl)
+                    .setName(this.labels.details.name[this.plugin.settings.language])
+                    .setDesc(this.labels.details.desc[this.plugin.settings.language])
+                    .addToggle((toggle) =>
+                        toggle
+                            .setValue(this.plugin.settings.showDetails)
+                            .onChange(async (value) => {
+                                this.plugin.settings.showDetails = value;
+                                await this.plugin.saveSettings();
+                            })
+                    )
+            }
+        )
+
+        this.settings.push(
+            {
+                key: this.labels.autoConvert,
+                value: new Setting(containerEl)
+                    .setName(this.labels.autoConvert.name[this.plugin.settings.language])
+                    .setDesc(this.labels.autoConvert.desc[this.plugin.settings.language])
+                    .addToggle((toggle) =>
+                        toggle
+                            .setValue(this.plugin.settings.autoConvert)
+                            .onChange(async (value) => {
+                                this.plugin.settings.autoConvert = value;
+                                await this.plugin.saveSettings();
+                            })
+                    )
+            }
+        )
+
+        this.settings.push(
+            {
+                key: this.labels.NonBoundaryCheckers,
+                value: new Setting(containerEl)
+                    .setName(this.labels.NonBoundaryCheckers.name[this.plugin.settings.language])
+                    .setDesc(this.labels.NonBoundaryCheckers.desc[this.plugin.settings.language])
+            }
+        );
+
+        let boundaryInput = containerEl.createDiv({ cls: "one-step-wikilink-setting-exclude-busyo" });
+        boundaryInput.contentEditable = "plaintext-only";
+        boundaryInput.textContent = this.plugin.settings.NonBoundaryCheckers.join(",");
+
+        boundaryInput.addEventListener('input', async () => {
+            this.plugin.settings.NonBoundaryCheckers = (boundaryInput.textContent as string).replace("，", ",").split(",").filter(keyword => keyword !== "");
+            await this.plugin.saveSettings();
+        });
+
+        this.settings.push(
+            {
+                key: this.labels.excludes,
+                value: new Setting(containerEl)
+                    .setName(this.labels.excludes.name[this.plugin.settings.language])
+                    .setDesc(this.labels.excludes.desc[this.plugin.settings.language])
+            }
+        );
+
+        let excludeInput = containerEl.createDiv({ cls: "one-step-wikilink-setting-exclude-busyo" });
+        excludeInput.contentEditable = "plaintext-only";
+        excludeInput.textContent = this.plugin.settings.excludes.join(",");
+
+        excludeInput.addEventListener('input', async () => {
+            this.plugin.settings.excludes = (excludeInput.textContent as string).replace("，", ",").split(",").filter(keyword => keyword !== "");
+            await this.plugin.saveSettings();
+        });
+    }
+
+    updateLanguage() {
+        for (let setting of this.settings) {
+            setting.value.setName(setting.key.name[this.plugin.settings.language]);
+            setting.value.setDesc(setting.key.desc[this.plugin.settings.language]);
+        }
+    }
+}
